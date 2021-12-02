def36d0eb031175a87c4632c65e72d77e658df05
Cantoni <fabio.cantoni@accenture.com>
2021-12-02 12:31:24 +0100

    first commit

---
+++ ui/widgets/conference/formWidget/src/components/__mocks__/conferenceMocks.js
@@ -0,0 +1,8 @@
+const conferenceMock = {
+  name:
+    'Voluptas quia maxime dolor repudiandae facere. Magni quam voluptatem odio iure provident praesentium. Ratione voluptatem molestiae deserunt sunt repellat. Nemo molestias accusantium maiores iusto laboriosam velit delectus. Consequuntur rem eos adipisci quis reprehenderit est.',
+  location:
+    'Natus et eligendi blanditiis voluptas. Labore quis doloremque minima. Autem et dolores. Dolorem cum molestiae nulla. Temporibus quos placeat perferendis optio atque laboriosam eum dolor reprehenderit.',
+};
+
+export default conferenceMock;
---
+++ ui/widgets/conference/tableWidget/src/components/__mocks__/conferenceMocks.js
@@ -0,0 +1,18 @@
+const conferenceMocks = [
+  {
+    id: 0,
+    name:
+      'Voluptas quia maxime dolor repudiandae facere. Magni quam voluptatem odio iure provident praesentium. Ratione voluptatem molestiae deserunt sunt repellat. Nemo molestias accusantium maiores iusto laboriosam velit delectus. Consequuntur rem eos adipisci quis reprehenderit est.',
+    location:
+      'Natus et eligendi blanditiis voluptas. Labore quis doloremque minima. Autem et dolores. Dolorem cum molestiae nulla. Temporibus quos placeat perferendis optio atque laboriosam eum dolor reprehenderit.',
+  },
+  {
+    id: 1,
+    name:
+      'Sed sed atque dolorem aut omnis eos aut. Aliquid occaecati tempore. Amet sint qui quidem eligendi aut rerum ipsa. Aut aut nihil aliquam.',
+    location:
+      'Illum aut suscipit enim ipsum quae. Ut ipsa iste reprehenderit ab ratione illum cupiditate et. Qui aperiam quas qui eligendi dicta saepe iure atque.',
+  },
+];
+
+export default conferenceMocks;
